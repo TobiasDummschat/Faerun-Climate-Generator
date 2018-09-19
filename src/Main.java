4ea75dbde31261560d5182a16a8b4c636118dd29
Tobi <c4rgon4t@gmail.com>
2018-09-19 10:30:19 +0200

    Improved exception messages

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.google.gson.*;
 
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,46 +8,47 @@ import java.util.Scanner;
 
 public class Main
 {
+    private static final String JSON_PATH = "generated_climate.json";
 
     //Array with lowest temperatures per region and month matching input in main()
-    // lowestTemps[0] is Near Spine of the World
-    // lowestTemps[1] is Subarctic
-    // lowestTemps[2] is Temperate
-    private static final int[][] lowestTemps = {
+    // LOWEST_TEMPS[0] is Near Spine of the World
+    // LOWEST_TEMPS[1] is Subarctic
+    // LOWEST_TEMPS[2] is Temperate
+    private static final int[][] LOWEST_TEMPS = {
             {-13, -14, -9, -3, +2, +7, +10, +9, +5, -1, -6, -9},
             {-10, -11, -7, -2, +3, +9, +12, +11, +6, +0, -3, -7},
             {-2, -3, -1, +2, +6, +10, +12, +12, +9, +5, +2, +1}};
 
-    private static final String[] monthDescriptions = {
-            "1st: Hammer (Deepwinter)", "2nd: Alturiak (The Claws of the Cold", "3rd: Ches (The Claw of the Sunsets",
-            "4th: Tarsakh (The Claw of the Storms)", "5th: Mirtul (The Melting)", "6th: Kythorn (The Time of Flowers",
+    private static final String[] MONTH_DESCRIPTIONS = {
+            "1st: Hammer (Deepwinter)", "2nd: Alturiak (The Claws of the Cold)", "3rd: Ches (The Claw of the Sunsets)",
+            "4th: Tarsakh (The Claw of the Storms)", "5th: Mirtul (The Melting)", "6th: Kythorn (The Time of Flowers)",
             "7th: Flamerule (Summertide)", "8th: Eleasias (Highsun)", "9th: Eleint (The Fading)",
             "10th: Marpennoth (Leafall)", "11th: Uktar (The Rotting)", "12th: Nightal (The Drawing Down)"};
 
-    private static final String[] regionDescriptions = {"1: Near Spine of the World", "2: Subarctic", "3: Temperate"};
+    private static final String[] REGION_DESCRIPTIONS = {"1: Near Spine of the World", "2: Subarctic", "3: Temperate"};
 
-    //JSON format: {region: {year: {month: {"temperature": int[30], "wind": int[30], "precipitation": int[30]}}}}
-    private static JSONObject _climateObj;
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    //Json format: {year: {month: {day: {"temperature": int, "wind": int, "precipitation": int, "saveDC": }}}}
+    private static JsonObject climateObj;
 
     public static void main(String[] args)
     {
-        setUpJSON();
+        setUpJson();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the current year: ");
         int year = scanner.nextInt();
 
-        System.out.println(_climateObj.isEmpty());
-        System.out.println(_climateObj.containsKey("" + year));
-        if (_climateObj.isEmpty() || !_climateObj.containsKey("" + year))
+        if (!climateObj.has("" + year))
             generateWeather(year);
 
-        for (String month : monthDescriptions) System.out.println(month);
+        for (String month : MONTH_DESCRIPTIONS) System.out.println(month);
         System.out.println("Enter the current month: ");
         int monthIndex = scanner.nextInt() - 1;
 
-        for (String region : regionDescriptions) System.out.println(region);
+        for (String region : REGION_DESCRIPTIONS) System.out.println(region);
 
         System.out.println("\nEnter current region:");
         int regionIndex = scanner.nextInt() - 1;
@@ -59,41 +59,41 @@ public class Main
     private static void generateWeather(int year)
     {
 
-        JSONObject yearObj = new JSONObject();
+        JsonObject yearObj = new JsonObject();
 
         for (int month = 0; month < 12; month++)
         {
-            JSONObject monthObj = new JSONObject();
+            JsonObject monthObj = new JsonObject();
 
             for (int day = 0; day < 30; day++)
             {
-                JSONObject dayObj = new JSONObject();
+                JsonObject dayObj = new JsonObject();
 
                 for (int region = 0; region < 3; region++)
                 {
-                    int temperature = rollDie(8) + lowestTemps[region][month];
+                    int temperature = rollDie(8) + LOWEST_TEMPS[region][month];
                     int windStrength = parseWeatherD20(rollDie(20));
                     int precipitationStrength = parseWeatherD20(rollDie(20));
 
                     int saveDC = 5 - temperature + 3 * (windStrength + precipitationStrength);
 
-                    JSONObject regionObj = new JSONObject();
+                    JsonObject regionObj = new JsonObject();
 
-                    regionObj.put("temperature", temperature);
-                    regionObj.put("wind", windStrength);
-                    regionObj.put("precipitation", precipitationStrength);
-                    regionObj.put("saveDC", saveDC);
+                    regionObj.addProperty("temperature", temperature);
+                    regionObj.addProperty("wind", windStrength);
+                    regionObj.addProperty("precipitation", precipitationStrength);
+                    regionObj.addProperty("saveDC", saveDC);
 
-                    dayObj.put(regionDescriptions[region], regionObj);
+                    dayObj.add(REGION_DESCRIPTIONS[region], regionObj);
                 }
 
-                monthObj.put(day, dayObj);
+                monthObj.add("" + day, dayObj);
             }
 
-            yearObj.put(month, monthObj);
+            yearObj.add("" + month, monthObj);
         }
-        _climateObj.put(year, yearObj);
-        writeJSON(_climateObj);
+        climateObj.add("" + year, yearObj);
+        writeJson(climateObj);
     }
 
     private static int parseWeatherD20(int roll)
@@ -108,39 +108,33 @@ public class Main
         return (int) Math.ceil(Math.random() * numberOfSides);
     }
 
-    private static void parseJSONFile()
+    private static JsonObject parseJsonFile(String path) throws FileNotFoundException
     {
-        JSONParser parser = new JSONParser();
+        JsonParser parser = new JsonParser();
+        return (JsonObject) parser.parse(new FileReader(path));
+    }
+
+    private static void setUpJson()
+    {
         try
         {
-            _climateObj = (JSONObject) parser.parse(new FileReader("generated_climate.json"));
-        } catch (Exception e)
+            climateObj = parseJsonFile(JSON_PATH);
+        } catch (FileNotFoundException ignored)
         {
-            System.out.println(e);
+            JsonObject obj = new JsonObject();
+            climateObj = obj;
+            writeJson(obj);
         }
     }
 
-    private static void setUpJSON()
+    private static void writeJson(JsonElement obj)
     {
-        File json = new File("generated_climate.json");
-        if (json.exists())
-            parseJSONFile();
-        else
+        try (FileWriter writer = new FileWriter(JSON_PATH))
         {
-            JSONObject obj = new JSONObject();
-            _climateObj = obj;
-            writeJSON(obj);
-        }
-    }
-
-    private static void writeJSON(JSONObject obj)
-    {
-        try (FileWriter writer = new FileWriter("generated_climate.json"))
+            writer.write(gson.toJson(obj));
+        } catch (IOException fileWritingException)
         {
-            writer.write(obj.toJSONString());
-        } catch (IOException e)
-        {
-            System.out.println(e);
+            System.err.println("Writing JsonElement to file has failed.\n" + fileWritingException);
         }
     }
 
